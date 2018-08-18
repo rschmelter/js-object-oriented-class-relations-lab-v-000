1ba75dc39875fcf9d11efefa18e3fb658bea7879
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-18 17:48:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+let store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name){
+    this.name = name
+    this.id = ++driverId
+
+    store.drivers.push(this)
+  }
+}
+
+class Passenger {
+  constructor(name){
+    this.name = name
+    this.id = ++passengerId
+
+    store.passengers.push(this)
+  }
+}
+class Trip {
+  constructor(driver, passenger){
+    this.driver = driver
+    this.passenger = passenger
+    this.id = ++tripId
+
+    if(driver){
+      this.driverId = driver.id
+    }
+
+    if(passenger){
+      this.passengerId = passenger.id
+    }
+
+    store.trips.push(this)
+  }
+}
