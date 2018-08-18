7c2fed7825a2920c115ee080336e1624a013681d
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-18 18:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ class Driver {
     this.id = ++driverId
 
     store.drivers.push(this)
-    trips() {
-  return store.trips.filter(trip => {
-    return trip.driverId == this.id;
-  });
+
   }
 }
 
