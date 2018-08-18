e5b71c698446315869f119bf3b2a546b25836cce
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-18 17:52:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,10 +37,6 @@ class Trip {
 
 
     store.trips.push(this)
-    passenger() {
-      return store.passengers.find(passenger => {
-        return passenger.id === this.passengerId;
-      });
-    }
+
   }
 }
