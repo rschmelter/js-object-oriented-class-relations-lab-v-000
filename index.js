801c94313029d1199c51edf2c497c5c92b7a34eb
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-18 18:13:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ class Trip {
     this.passengerId = passenger.id
 
     store.trips.push(this)
-    passenger(){
-      return store.passengers.find(function(passenger){
-        return passenger.id === passengerId
-      })
+    // passenger(){
+    //   return store.passengers.find(function(passenger){
+    //     return passenger.id === passengerId
+    //   })
     }
   }
 }
